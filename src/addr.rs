26e02230fde4f026d62cf63bb498f83a16e2d9c7
Zavier Divelbiss <semiviral@gmail.com>
2022-12-31 00:51:16 -0600

    improve `Address` type

--- src/addr.rs
+++ src/addr.rs
@@ -1,41 +1,34 @@
 pub type Frame = AlignedAddress<0x1000>;
-pub type Page = AlignedAddress<0x1000>;
 pub type Address = AlignedAddress<0x1>;
 
 #[repr(transparent)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct AlignedAddress<const ALIGN: usize>(usize);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct AlignedAddress<const ALIGN_SHIFT: u32>(usize);
 
-impl<const ALIGN: usize> AlignedAddress<ALIGN> {
-    const NON_CANONICAL_BITS: usize = 0xFFF00000_00000000;
-    const CANONICAL_BITS: usize = !Self::NON_CANONICAL_BITS;
+impl<const ALIGN_SHIFT: u32> AlignedAddress<ALIGN_SHIFT> {
+    const NON_CANONICAL_MASK: usize = 0xFFF00000_00000000;
+    const ALIGN_MASK: usize = 1usize.checked_shl(ALIGN_SHIFT).unwrap_or(0).wrapping_sub(1);
+
+    #[inline]
+    pub const fn is_canonical(address: usize) -> bool {
+        (address & Self::NON_CANONICAL_MASK) == 0
+    }
 
     #[inline]
     pub const fn zero() -> Self {
         Self(0)
     }
 
-    #[inline]
-    pub const fn is_canonical(address: usize) -> bool {
-        (address & Self::NON_CANONICAL_BITS) == 0
-    }
-
     /// Constructs a new `Address<Physical>` if the provided address is canonical.
     #[inline]
     pub fn new(address: usize) -> Option<Self> {
-        debug_assert!(ALIGN.is_power_of_two());
-
-        (Self::is_canonical(address) && address).then_some(Self(address))
+        (Self::is_canonical(address) && ((address & Self::ALIGN_MASK) == 0))
+            .then_some(Self(address))
     }
 
     #[inline]
     pub const fn new_truncate(address: usize) -> Self {
-        Self(address & Self::CANONICAL_BITS)
-    }
-
-    #[inline]
-    pub const fn is_frame_aligned(self) -> bool {
-        (self.0 & 0xFFF) == 0
+        Self(address & !Self::NON_CANONICAL_MASK & !Self::ALIGN_MASK)
     }
 }
 
